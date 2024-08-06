be06427a8534ab70eaaf5cd02e422b97b55a61d1
Aguss261 <261draft@gmail.com>
2024-08-06 02:46:20 -0300

    fix docker

--- src/database/database.go
+++ src/database/database.go
@@ -4,20 +4,25 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"os"
 )
 
 var db *sql.DB
 
 func Connect() (*sql.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_HOST"),
-		"3306", // puerto de MySQL, puedes usar el puerto por defecto
-		os.Getenv("MYSQL_DATABASE"))
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, err
+	}
+	dns := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
 
-	db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", dns)
 	if err != nil {
 		return nil, err
 	}
